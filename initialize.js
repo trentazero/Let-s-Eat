3d9631e01c9d7b7eacf3ae1c6c36fd5aea8cc7a8
HydraOpt <a.gadaleta@rgu.ac.uk>
2017-04-26 11:38:43 +0100

    undo

--- initialize.js
+++ initialize.js
@@ -12,7 +12,6 @@ function initMap(){
     // could have set it directly with {lat: -34.397, lng: 150.644}
     center: currPosition,
     zoom: 13
-    mapTypeId: HYBRID
   });
 
   //initialize infoWindow as a InfoWindow object (baloon on top of the place marker)

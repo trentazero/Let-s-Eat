2cae85431a04cdcbed0ca6571aeacd56b3776dce
HydraOpt <a.gadaleta@rgu.ac.uk>
2017-04-26 17:53:38 +0100

    solved?

--- initialize.js
+++ initialize.js
@@ -98,10 +98,10 @@ function initMap(){
       }
       //console.log(place);
       //currPosition = new google.maps.LatLng(((bounds.b.b + bounds.b.f)/2), ((bounds.f.b + bounds.f.f)/2));
-      var newLat = ((bounds.b.b + bounds.b.f)/2);
-      var newLng = ((bounds.f.b + bounds.f.f)/2)
+      var newLng = ((bounds.b.b + bounds.b.f)/2);
+      var newLat = ((bounds.f.b + bounds.f.f)/2)
 
-      currPosition = new google.maps.LatLng(-37.8182574, 144.9658713);
+      currPosition = new google.maps.LatLng(newLat, newLng);
       console.log(newLat);
       console.log(newLng);
 

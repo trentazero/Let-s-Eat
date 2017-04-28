d0b0bc235afabd443fe3e68618b23be41577b70f
HydraOpt <a.gadaleta@rgu.ac.uk>
2017-04-28 02:00:17 +0100

    remix (link worked before)

---
+++ konami.js
@@ -0,0 +1,14 @@
+/* Additional code from the Magnific Popup API
+source: http://dimsemenov.com/plugins/magnific-popup/ */
+// Magnific Popup v1.1.0 by Dmitry Semenov
+// http://bit.ly/magnific-popup#build=inline+image+ajax+iframe+gallery+retina+imagezoom
+$(document).ready(function() {
+	$('.konami_popup').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false
+	});
+});
